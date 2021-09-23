ed183e89d50fc9c0df008fe582e977c34da96166
iHusk <tjhayes@unomaha.edu>
2021-09-22 22:50:39 -0500

    getting rid of 'fix for email'. I had it right the first time.

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
   ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
